d221e843f1fdcf3f04342136abf5bf4d439c0f61
Luiz Vinicius <viniciusadr244@gmail.com>
2024-09-05 11:17:11 -0300

    first commit

---
+++ src/lib/firebase/firebase-secret.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.apikey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+}
+
+const app = getApps(). length == 0 ? initializeApp (firebaseConfig) : getApp()
+export const db = getFirestore(app);
+export const storage = getStorage(app);
