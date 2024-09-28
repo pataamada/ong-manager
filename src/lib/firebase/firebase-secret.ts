c922df93204773a43ec23df79ae383fdb9c7d56d
Luiz Vinicius <viniciusadr244@gmail.com>
2024-09-28 09:50:47 -0300

    feat: update biome rules

--- src/lib/firebase/firebase-secret.ts
+++ src/lib/firebase/firebase-secret.ts
@@ -1,17 +1,17 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from 'firebase/storage';
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.apikey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+	apiKey: process.env.apikey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
+	measurementId: process.env.measurementId
 }
 
-const app = getApps(). length == 0 ? initializeApp (firebaseConfig) : getApp()
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
+export const db = getFirestore(app)
+export const storage = getStorage(app)
