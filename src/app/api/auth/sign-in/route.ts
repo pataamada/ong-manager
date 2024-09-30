307fa66bf52fd7515179c11b1908d5b2acaa54e4
Luiz Vinicius <viniciusadr244@gmail.com>
2024-09-30 15:12:47 -0300

    feat: add role to token

--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -1,23 +1,35 @@
-import { cookies } from "next/headers";
-import { type NextRequest, NextResponse } from "next/server";
-import { createSessionCookie } from "@/lib/firebase/firebase-admin";
+import { cookies } from "next/headers"
+import { type NextRequest, NextResponse } from "next/server"
+import { createSessionCookie, firebaseApp } from "@/lib/firebase/firebase-admin"
+import { findOne } from "@/services/user.service"
+import { getAuth } from "firebase-admin/auth"
+import type { User } from "@/models/user.model"
 
 export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const idToken = body.idToken;
+	const body = await request.json()
+	const idToken = body.idToken
+	const userId = body.id
 
-    const expiresIn =  1 * 60 * 60 * 1000; // 1 hour
+	const userDb: Partial<User> | undefined = await findOne(userId)
+	console.log(userDb)
 
-    const sessionCookie = await createSessionCookie(idToken, { expiresIn });
+	const additionalClaims = {
+		role: userDb!.role,
+	}
+	const token = await getAuth(firebaseApp).createCustomToken(userId, additionalClaims)
+	console.log(token)
 
-    cookies().set("__session", sessionCookie, {
-        maxAge: expiresIn,
-        httpOnly: true,
-        secure: true,
-    });
+	const expiresIn = 1 * 60 * 60 * 1000 // 1 hour
+	const sessionCookie = await createSessionCookie(idToken, { expiresIn })
 
-    return NextResponse.json({
-        success: true,
-        data: "Signed in successfully.",
-    });
+	cookies().set("__session", sessionCookie, {
+		maxAge: expiresIn,
+		httpOnly: true,
+		secure: true,
+	})
+
+	return NextResponse.json({
+		success: true,
+		data: "Signed in successfully.",
+	})
 }
--- src/auth/useAuth/index.tsx
+++ src/auth/useAuth/index.tsx
@@ -2,16 +2,15 @@ import { app, auth } from "@/lib/firebase/firebase-secret"
 import { FirebaseError } from "firebase/app"
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { setCookie } from "cookies-next"
-import { findOne } from "@/services/user.service"
-import { getAuth } from "firebase-admin/auth"
+
 type LoginPayload = {
 	email: string
 	password: string
 }
+
 export async function login(data: LoginPayload) {
 	try {
 		const { user } = await signInWithEmailAndPassword(auth, data.email, data.password)
-
 
 		if (app.name) {
 			console.log(JSON.stringify(user))
@@ -28,24 +27,13 @@ export async function signInTest(data: LoginPayload) {
 	try {
 		const { user } = await signInWithEmailAndPassword(auth, data.email, data.password)
 		const idToken = await user.getIdToken()
-		const userDb = await findOne(user.uid)
-		console.log(userDb)
-		
-		const userId = user.uid
-		const additionalClaims = {
-			role: true,
-		};
-		const testAuth = getAuth()
-		const token = await testAuth.createCustomToken(userId, additionalClaims)
-		console.log(token);
-		
 
 		const response = await fetch("/api/auth/sign-in", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ idToken }),
+			body: JSON.stringify({ idToken, id: user.uid }),
 		})
 		const body = await response.json()
 		if (response.ok && body.success) {
@@ -53,7 +41,7 @@ export async function signInTest(data: LoginPayload) {
 		}
 		return false
 	} catch (error) {
-		console.error("Error signing in with Google", error)
+		console.error("Invalid credentials", error)
 		return false
 	}
 }
