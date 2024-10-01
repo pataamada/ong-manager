2ac9511c650d3378915eaa797a65825a27f3e916
Luiz Vinicius <viniciusadr244@gmail.com>
2024-10-01 11:29:53 -0300

    feat: single login

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,16 +1,15 @@
 "use server"
 
-import { z } from "zod"
+import { custom, z } from "zod"
 import { actionClient } from "@/actions/safe-action"
 import { signInWithCustomToken, signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "@/lib/firebase/firebase-secret"
 import { createSessionCookie, firebaseApp } from "@/lib/firebase/firebase-admin"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
-import { findOne } from "@/services/user.service"
+import { findUserByLogin } from "@/services/user.service"
 import { getAuth } from "firebase-admin/auth"
-import type { User } from "@/models/user.model"
-// This schema is used to validate input from client.
+
 const schema = z.object({
 	email: z.string().email(),
 	password: z.string().min(8).max(100),
@@ -19,15 +18,16 @@ const schema = z.object({
 export const login = actionClient
 	.schema(schema)
 	.action(async ({ parsedInput: { email, password } }) => {
-		const { user } = await signInWithEmailAndPassword(auth, email, password)
-		const userId = user.uid
-		const userDb: Partial<User> | undefined = await findOne(userId)
+		const userDb = await findUserByLogin(email, password)
+		console.log(userDb)
+		const userId = userDb.id
+
 		const additionalClaims = {
-			role: userDb?.role || "AUTHENTICATED",
+			role: userDb?.data.role || "AUTHENTICATED",
 		}
-		const token = await getAuth(firebaseApp).createCustomToken(userId, additionalClaims)
-		const customToken = await signInWithCustomToken(auth, token)
-		const tokenId = await customToken.user.getIdToken()
+		const customToken = await getAuth(firebaseApp).createCustomToken(userId, additionalClaims)
+		const token = await signInWithCustomToken(auth, customToken)
+		const tokenId = await token.user.getIdToken()
 		const expiresIn = 5 * 60 * 60 * 1000 // 1 hour
 		const sessionCookie = await createSessionCookie(tokenId, { expiresIn })
 		cookies().set("__session", sessionCookie, {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,20 @@
-import { db } from "@/lib/firebase/firebase-secret";
-import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/lib/firebase/firebase-secret"
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore"
 
 export const findOne = async (id: string) => {
-    const document = await getDoc(doc(db, `users/${id}`));
-    return document.data()
-};
+	const document = await getDoc(doc(db, `users/${id}`))
+	return document.data()
+}
+
+export const findUserByLogin = async (email: string, password: string) => {
+	const q = query(
+		collection(db, "users"),
+		where("email", "==", email),
+		where("password", "==", password),
+	)
+	const querySnapshot = await getDocs(q)
+	const doc = querySnapshot.docs.map(doc => {
+		return { id: doc.id, data: doc.data() }
+	})
+	return doc[0]
+}
