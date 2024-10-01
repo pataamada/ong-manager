6ddf503f9651ee6c1e5f2e57b991002cfde09574
zignaggo <jvz1@aluno.ifal.edu.br>
2024-10-01 11:51:36 -0300

    feat: handle forbidden error

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -1,65 +1,53 @@
-import "server-only";
-import { type SessionCookieOptions, getAuth } from "firebase-admin/auth";
-import {
-    initializeApp,
-    getApps,
-    cert,
-    type ServiceAccount,
-} from "firebase-admin/app";
-import { envServerSchema } from "@/types/env-schema";
-import { getSession } from "../session";
+import "server-only"
+import { type SessionCookieOptions, getAuth } from "firebase-admin/auth"
+import { initializeApp, getApps, cert, type ServiceAccount } from "firebase-admin/app"
+import { envServerSchema } from "@/types/env-schema"
+import { getSession } from "../session"
 
 export const adminService: ServiceAccount = {
-    projectId: envServerSchema.projectId,
-    clientEmail: envServerSchema.firebaseAdminClientEmail,
-    privateKey: envServerSchema.firebaseAdminPrivateKey,
-};
+	projectId: envServerSchema.projectId,
+	clientEmail: envServerSchema.firebaseAdminClientEmail,
+	privateKey: envServerSchema.firebaseAdminPrivateKey,
+}
 export const firebaseApp =
-    getApps().find((it) => it.name === "firebase-admin-app") ||
-    initializeApp(
-        {
-            credential: cert(adminService),
-        },
-        "firebase-admin-app"
-    );
-export const auth = getAuth(firebaseApp);
+	getApps().find(it => it.name === "firebase-admin-app") ||
+	initializeApp(
+		{
+			credential: cert(adminService),
+		},
+		"firebase-admin-app",
+	)
+export const auth = getAuth(firebaseApp)
 
-export async function isUserAuthenticated(
-    session: string | undefined = undefined
-) {
-    const _session = session ?? (await getSession());
-    if (!_session) return false;
-    try {
-        const isRevoked = await auth.verifySessionCookie(_session, true);
-        return !!isRevoked;
-    } catch (error) {
-        return false;
-    }
+export async function isUserAuthenticated(session: string | undefined = undefined) {
+	const _session = session ?? (await getSession())
+	if (!_session) return false
+	try {
+		const isRevoked = await auth.verifySessionCookie(_session, true)
+		return !!isRevoked
+	} catch (error) {
+		return false
+	}
 }
 
 export async function getCurrentUser() {
-    const session = await getSession();
+	const session = await getSession()
+	if (!(await isUserAuthenticated(session))) {
+		return null
+	}
 
-    if (!(await isUserAuthenticated(session))) {
-        return null;
-    }
+	const decodedIdToken = await auth.verifySessionCookie(session!)
+	const currentUser = await auth.getUser(decodedIdToken.uid)
 
-    const decodedIdToken = await auth.verifySessionCookie(session!);
-    const currentUser = await auth.getUser(decodedIdToken.uid);
-
-    return currentUser;
+	return { user: currentUser, role: decodedIdToken.role }
 }
 
-
-export function createSessionCookie(
-    idToken: string,
-    sessionCookieOptions: SessionCookieOptions
-) {
-    return auth.createSessionCookie(idToken, sessionCookieOptions);
+export function createSessionCookie(idToken: string, sessionCookieOptions: SessionCookieOptions) {
+	return auth.createSessionCookie(idToken, sessionCookieOptions)
 }
 
 export async function revokeAllSessions(session: string) {
-    const decodedIdToken = await auth.verifySessionCookie(session);
+	const decodedIdToken = await auth.verifySessionCookie(session)
 
-    return await auth.revokeRefreshTokens(decodedIdToken.sub);
+	return await auth.revokeRefreshTokens(decodedIdToken.sub)
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,7 +1,11 @@
+export enum UserRoles {
+    Admin = 'ADMIN',
+    Authenticated = 'AUTHENTICATED'
+}
 export interface User {
     name: string;
     cpf: string;
-    role: string;
+    role: UserRoles;
     email: string;
     uid: string;
     photo: string;
