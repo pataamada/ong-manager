a67d8b972da0b547bf78ef99fe97671678db0039
Arthur Clemente <arthurclementte@hotmail.com>
2024-10-05 17:36:28 -0300

    feat: created animal model

---
+++ src/models/animal.model.ts
@@ -0,0 +1,14 @@
+export interface Animal {
+  animalid: number;
+  name: string;
+  age: number;
+  type: string;
+  sex: string;
+  size: string;
+  photo: string[];
+  observations: string;
+  castration: boolean;
+  vaccines: string[];
+  available: boolean;
+  birthDate: Date;
+}
