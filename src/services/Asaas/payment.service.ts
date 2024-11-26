cac159b03941e95d665a853eb8813e772177d979
Arthur Clemente <arthurclementte@hotmail.com>
2024-11-26 11:25:36 -0300

    feat: implement the function to create the payment

---
+++ src/services/Asaas/payment.service.ts
@@ -0,0 +1,27 @@
+import { Api } from "@/lib/axiosConfig"
+import type {
+	IPayment,
+	IPaymentCreateBoletoOrPix,
+	IPaymentCreateCreditCard,
+} from "@/types/Asaas/Payment"
+
+const createPayment = async (
+	form: IPaymentCreateBoletoOrPix | IPaymentCreateCreditCard,
+): Promise<IPayment | Error> => {
+	try {
+		const urlRelative = `/payments?type=${form.billingType}`
+		const { data } = await Api.post<IPayment>(urlRelative, form)
+		if (data) return data
+
+		return new Error("Erro ao criar cobrança.")
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	} catch (error: any) {
+		console.log(error)
+
+		return new Error((error as { message: string }).message || "Erro ao criar cobrança.")
+	}
+}
+
+export const PaymentService = {
+	createPayment,
+}
