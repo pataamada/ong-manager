1d90b3dffe2d79cfc826c4cdfdb00ac08d2dda6c
Arthur Clemente <arthurclementte@hotmail.com>
2024-11-26 11:26:40 -0300

    feat: implement the functions to save the donation and expense info.

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -10,6 +10,8 @@ import {
 	deleteDoc,
 } from "firebase/firestore"
 import { db, storage } from "@/lib/firebase/firebase-secret"
+import { PaymentService } from "@/services/Asaas/payment.service"
+import type { IPaymentCreateBoletoOrPix } from "@/types/Asaas/Payment"
 
 export const saveDonationDb = async (
 	userId: string | undefined,
@@ -18,7 +20,27 @@ export const saveDonationDb = async (
 	value: number,
 	description: string,
 	date: string,
-) => {}
+) => {
+  const donationData: IPaymentCreateBoletoOrPix = {
+    billingType: 'BOLETO',
+    value: value,
+    customer: userId,
+    dueDate: date,
+  }
+
+  const createdDonation = await PaymentService.createPayment(donationData)
+
+  const document = await addDoc(collection(db, "donations"), {
+    userId: userId,
+    animalId: animalId,
+    category: category,
+    value: value,
+    description: description,
+    date: date,
+    donation: createdDonation,
+  })
+  return JSON.stringify(document)
+}
 
 export const saveExpenseDb = async (
 	userId: string,
@@ -27,4 +49,24 @@ export const saveExpenseDb = async (
 	proof: string[],
 	description: string,
 	date: string,
-) => {}
+) => {
+  const expenseData: IPaymentCreateBoletoOrPix = {
+    billingType: 'BOLETO',
+    value: value,
+    customer: userId,
+    dueDate: date,
+  }
+
+  const createExpense = await PaymentService.createPayment(expenseData)
+
+  const document = await addDoc(collection(db, "expenses"), {
+    userId: userId,
+    category: category,
+    value: value,
+    description: description,
+    proof: proof,
+    date: date,
+    expense: createExpense,
+  })
+  return JSON.stringify(document)
+}
