a9f3ad4812186b5db1597b1c2ceca1d1231f1229
Luiz Vinicius <viniciusadr244@gmail.com>
2024-12-09 17:52:53 -0300

    style: biome format

--- src/types/processEnv.ts
+++ src/types/processEnv.ts
@@ -1,13 +1,11 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-// biome-ignore lint/style/useImportType: <explanation>
-import { z } from "zod";
-import type { envSchema } from "./env-schema";
+import type { z } from "zod"
+import type { envSchema } from "./env-schema"
 
-type EnvSchemaType = z.infer<typeof envSchema>;
+type EnvSchemaType = z.infer<typeof envSchema>
 
 declare global {
-    // biome-ignore lint/style/noNamespace: <explanation>
-    namespace NodeJS {
-        interface ProcessEnv extends EnvSchemaType {}
-    }
+	// biome-ignore lint/style/noNamespace: <explanation>
+	namespace NodeJS {
+		interface ProcessEnv extends EnvSchemaType {}
+	}
 }
