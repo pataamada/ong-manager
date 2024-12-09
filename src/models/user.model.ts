5a9e1b0505f8666b92f42d5a5c28d065f7ed5046
Luiz Vinicius <viniciusadr244@gmail.com>
2024-12-09 18:03:32 -0300

    fex: remove unused properties

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -10,16 +10,14 @@ export interface User {
 	password: string
 	uid: string
 	photo?: string
-	address?: string | null
-	birthDate?: Date | null
-	phone?: string | null
+	phone?: string | null // todo: adicionar
 }
 
 export type UserWTempUid = User & { tempUid?: string | null }
 export type UserWOutPassword = Omit<UserWTempUid, "password">
 export type CreateUserPayload = Pick<User, "cpf">
 export const accessPageList: Record<UserRoles, string[]> = {
-	ADMIN: ['/dashboard', '/users','/finance'],
-	AUTHENTICATED: ['/animals', '/schedules'],
+	ADMIN: ["/dashboard", "/users", "/finance"],
+	AUTHENTICATED: ["/animals", "/schedules"],
 }
 export const publicPageList = ["/login", "/register", "/forgot-password"]
