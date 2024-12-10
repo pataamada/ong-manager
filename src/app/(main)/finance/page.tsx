ae8793b5e0cd8b681f2b6250b4544eed7014fe1e
Gabriel Moreira <gsm.br2003@gmail.com>
2024-12-10 07:32:15 -0300

    feat: add totalizer

--- src/app/(main)/finance/page.tsx
+++ src/app/(main)/finance/page.tsx
@@ -1,3 +1,30 @@
+import { IPropsTotalizer, Totalizer } from "./_components/Totalizer";
+
+const mockedTotalizers: IPropsTotalizer[] = [
+	{
+		type: 'total',
+		label: 'Total',
+		value: 'R$ 300,00'
+	},
+	{
+		type: 'donations',
+		label: 'Doações',
+		value: 'R$ 1600,00'
+	},
+	{
+		type: 'expenses',
+		label: 'Despesas',
+		value: 'R$ 1300,00'
+	},
+]
+
 export default function Finance() {
-	return <div>Finances</div>
+  return (
+    <div>
+      <div className="flex flex-wrap lg:flex-nowrap gap-4">
+       {mockedTotalizers.map(i => <Totalizer key={i.type} type={i.type} label={i.label} value={i.value} />)}
+      </div>
+      <div className="flex"></div>
+    </div>
+  );
 }
