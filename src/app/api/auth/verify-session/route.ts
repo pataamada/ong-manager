55312a5cdbba05bd9677c4bb8674ce19ee2016fd
Luiz Vinicius <viniciusadr244@gmail.com>
2024-12-13 10:36:22 -0300

    remove useless comments

--- src/app/api/auth/verify-session/route.ts
+++ src/app/api/auth/verify-session/route.ts
@@ -1,7 +1,7 @@
 import { auth, isUserAuthenticated, revokeAllSessions } from "@/lib/firebase/firebase-admin"
 import type { UserRoles } from "@/models/user.model"
 import { NextResponse } from "next/server"
-// request: Request
+
 export async function POST(request: Request) {
 	const body = await request.json()
 	const session = body.session
@@ -15,8 +15,8 @@ export async function POST(request: Request) {
 		}
 		const decodedIdToken = await auth.verifyIdToken(session!)
 		const currentUser = await auth.getUser(decodedIdToken.uid)
-        return NextResponse.json({ user: currentUser, role: decodedIdToken.role as UserRoles })
+		return NextResponse.json({ user: currentUser, role: decodedIdToken.role as UserRoles })
 	} catch (error) {
-        return NextResponse.json({ user: null, role: null })
-    }
+		return NextResponse.json({ user: null, role: null })
+	}
 }
