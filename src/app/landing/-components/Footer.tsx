d087c2cad931411e6a9e7d1cb58fe90a026b32fe
Eliezir <eliezir2004@gmail.com>
2024-12-15 22:03:13 -0300

    fix: footer minor style changes

--- src/app/landing/-components/Footer.tsx
+++ src/app/landing/-components/Footer.tsx
@@ -1,77 +1,115 @@
-"use client";
+"use client"
 
-import { Icon } from "@iconify/react";
-import Image from "next/image";
+import { Icon } from "@iconify/react"
+import Image from "next/image"
+
+const socials = [
+	{
+		name: "Instagram",
+		icon: "mdi:instagram",
+		link: "https://instagram.com",
+	},
+	{
+		name: "Facebook",
+		icon: "basil:facebook-outline",
+		link: "https://facebook.com",
+	},
+	{
+		name: "Telefone",
+		icon: "bi:telephone",
+		link: "tel:+5582987654321",
+	},
+]
+
+const sections = [
+	{ name: "about", label: "Sobre nós", link: "/#" },
+	{ name: "pets", label: "Conheça nossos peludos", link: "/#" },
+	{ name: "help", label: "Precisamos da sua ajuda", link: "/#" },
+	{ name: "transparency", label: "Transparência", link: "/#" },
+	{ name: "volunteers", label: "Voluntários", link: "#volunteers" },
+	{ name: "partners", label: "Parceiros", link: "#partners" },
+]
+
+const menu = [
+	{ name: "adoption", label: "Animais para adoção", link: "/#" },
+	{ name: "donations", label: "Doações", link: "/#" },
+	{ name: "schedule", label: "Agenda", link: "/#" },
+]
 
 export function Footer() {
-    return (
-        <footer className="bg-primary text-white py-8">
-            <div className="container mx-auto max-w-screen-2xl flex flex-wrap justify-between items-start gap-12 pl-4 md:pl-20">
-                <div className="w-full md:w-96 h-auto flex-shrink-0">
-                    <Image
-                        src="/landingPage/footer-images/icon.svg"
-                        alt="Logo"
-                        width={96}
-                        height={67}
-                        className="mb-4"
-                    />
-                    <p className="text-paragraph-5 mb-6">
-                        Somos a organização não governamental CãoDomínio, um abrigo que trabalha há 30 anos para fornecer assistência a cães e gatos necessitados.
-                        Estamos localizados em: Rua 17 de Agosto, 130, Centro, Satuba/AL - CEP 57120-000.
-                        Funcionamos de segunda à sexta, das 8 às 18:00.
-                    </p>
-                    <div className="flex justify-start md:justify-center gap-4">
-                        <a
-                            href="https://instagram.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-white hover:text-secondary transition-colors"
-                        >
-                            <Icon icon="mdi:instagram" className="text-2xl" />
-                        </a>
-                        <a
-                            href="https://facebook.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-white hover:text-secondary transition-colors"
-                        >
-                            <Icon icon="basil:facebook-outline" className="text-2xl" />
-                        </a>
-                        <a
-                            href="tel:+5582987654321"
-                            className="text-white hover:text-secondary transition-colors"
-                        >
-                            <Icon icon="bi:telephone" className="text-2xl" />
-                        </a>
-                    </div>
-                </div>
+	return (
+		<footer className="bg-primary text-white py-12 px-2">
+			<div className="container mx-auto max-w-screen-2xl flex flex-wrap justify-between md:items-center items-start gap-12 pl-4 md:pl-20">
+				<div className="w-full md:w-96 h-auto flex-shrink-0 lg:mr-[140px]">
+					<Image
+						src="/landingPage/footer-images/icon.svg"
+						alt="Logo"
+						width={96}
+						height={67}
+						className="mb-4"
+					/>
+					<p className="text-paragraph mb-6">
+						Somos a organização não governamental CãoDomínio, um abrigo que trabalha há 30 anos para
+						fornecer assistência a cães e gatos necessitados.
+					</p>
+					<p className="text-paragraph mb-6">
+						Estamos localizados em: Rua 17 de Agosto, 130, Centro, Satuba/AL - CEP 57120-000.
+						Funcionamos de segunda à sexta, das 8 às 18:00.
+					</p>
+					<div className="flex justify-start md:justify-center gap-4">
+						{socials.map((social, index) => (
+							<a
+								key={`${social.name}-${index}`}
+								href={social.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-white hover:text-secondary transition-colors"
+							>
+								<Icon icon={social.icon} className="text-[30px]" />
+							</a>
+						))}
+					</div>
+				</div>
 
-                <div className="flex-1 space-y-6 w-full md:w-auto">
-                    <h3 className="text-subtitle-2 mb-4 font-bold">Sessões</h3>
-                    <ul className="space-y-2">
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Sobre nós</a></li>
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Conheça nossos peludos</a></li>
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Precisamos da sua ajuda</a></li>
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Transparência</a></li>
-                        <li><a href="#volunteers" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Voluntários</a></li>
-                        <li><a href="#partners" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Parceiros</a></li>
-                    </ul>
-                </div>
+				<div className="flex-1 w-full md:w-auto min-h-[186px]">
+					<h3 className="text-subtitle-2 font-bold mb-2">Sessões</h3>
+					<ul className="space-y-1">
+						{sections.map((section, index) => (
+							<li key={`${section.name}-${index}`}>
+								<a
+									href={section.link}
+									className="text-paragraph hover:text-secondary transition-colors no-underline"
+								>
+									{section.label}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
 
-                <div className="flex-1 space-y-6 w-full md:w-auto">
-                    <h3 className="text-subtitle-2 mb-4 font-bold">Menu</h3>
-                    <ul className="space-y-2">
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Animais para adoção</a></li>
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Doações</a></li>
-                        <li><a href="/#" className="text-paragraph-5 hover:text-secondary transition-colors no-underline">Agenda</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="text-center mt-8">
-                <p className="text-paragraph-5">
-                    Copyright © {new Date().getFullYear()} CãoDomínio.<br />Todos os direitos reservados.
-                </p>
-            </div>
-        </footer>
-    );
+				<div className="flex-1 w-full md:w-auto min-h-[186px]">
+					<h3 className="text-subtitle-2 mb-2 font-bold">Menu</h3>
+					<ul className="space-y-1">
+						{menu.map((menuItem, index) => (
+							<li key={`${menuItem.name}-${index}`}>
+								<a
+									href={menuItem.link}
+									className="text-paragraph hover:text-secondary transition-colors no-underline"
+								>
+									{menuItem.label}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			<div className="text-center mt-8">
+				<p className="text-paragraph-5">
+					Copyright © {new Date().getFullYear()} CãoDomínio.
+					<br />
+					Todos os direitos reservados.
+				</p>
+			</div>
+		</footer>
+	)
 }
